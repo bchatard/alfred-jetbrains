049bdc25391cfabba5ee945dc5eb02dc51015e04
bchatard <brice@chatard.fr>
2019-04-11 15:55:41 +0200

    :bug: Issue with project name retrieved viaDirectoryName

--- src/project/name.js
+++ src/project/name.js
@@ -1,39 +1,47 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const getFiles = srcPath => fs.readdirSync(srcPath)
-  .filter(file => fs.lstatSync(path.join(srcPath, file)).isFile());
+const getFiles = srcPath =>
+  fs
+    .readdirSync(srcPath)
+    .filter(file => fs.lstatSync(path.join(srcPath, file)).isFile());
 
-const ideaDirExists = (projectPath) => {
+const ideaDirExists = projectPath => {
   const ideaPath = `${projectPath}/.idea`;
   return fs.existsSync(ideaPath) && fs.lstatSync(ideaPath).isDirectory();
 };
 
-const getProductName = (projectPath) => {
+const getProductName = projectPath => {
   let isIdeaDirExists = false;
 
   const cases = {
     viaName: () => {
       const viaPath = `${projectPath}/.idea/name`;
-      return isIdeaDirExists && fs.existsSync(viaPath) ? fs.readFileSync(viaPath).toString() : false;
+      return isIdeaDirExists && fs.existsSync(viaPath)
+        ? fs.readFileSync(viaPath).toString()
+        : false;
     },
     viaDotName: () => {
       const viaPath = `${projectPath}/.idea/.name`;
-      return isIdeaDirExists && fs.existsSync(viaPath) ? fs.readFileSync(viaPath).toString() : false;
+      return isIdeaDirExists && fs.existsSync(viaPath)
+        ? fs.readFileSync(viaPath).toString()
+        : false;
     },
     viaIml: () => {
       if (!isIdeaDirExists) {
         return false;
       }
       const viaPath = `${projectPath}/.idea`;
-      const imlFiles = getFiles(viaPath).filter(file => path.extname(file) === '.iml');
-      return (imlFiles.length === 1) ? path.basename(imlFiles[0], '.iml') : false;
+      const imlFiles = getFiles(viaPath).filter(
+        file => path.extname(file) === ".iml"
+      );
+      return imlFiles.length === 1 ? path.basename(imlFiles[0], ".iml") : false;
     },
     viaDirectoryName: () => {
       if (!isIdeaDirExists) {
         return false;
       }
-      return path.basename(path.dirname(projectPath));
+      return path.basename(projectPath);
     }
   };
 
