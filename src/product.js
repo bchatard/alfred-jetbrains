5c23d336434fb35c4ad83be5873b3eb3c0d03a48
Ibrahim Mubarak Hussain <47777720+ibrahim-mubarak@users.noreply.github.com>
2020-10-19 11:38:55 +0530

    Add Support for Android Studio 4.1

--- src/product.js
+++ src/product.js
@@ -58,6 +58,7 @@ const applyCustomisation = (product) => {
 const getPreferencePath = (product) => {
   // see: https://intellij-support.jetbrains.com/hc/en-us/articles/206544519-Directories-used-by-the-IDE-to-store-settings-caches-plugins-and-logs
   const preferencesPaths = [
+    `${process.env.HOME}/Library/Application Support/Google`, // >= Android Studio 4.1
     `${process.env.HOME}/Library/Application Support/JetBrains`, // >= 2020.*
     `${process.env.HOME}/Library/Preferences`, // < 2020.*
   ];
