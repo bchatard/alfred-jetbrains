91298f036fcacd98f3f8feb28662e0d026b41218
bchatard <brice@chatard.fr>
2020-12-02 20:14:44 +0100

    2020.3 compatibility
    fix #170

--- src/project/paths.js
+++ src/project/paths.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const fxp = require('fast-xml-parser/src/parser');
+const fs = require("fs");
+const fxp = require("fast-xml-parser/src/parser");
 
 const fxpOptions = {
   ignoreAttributes: false,
@@ -14,20 +14,25 @@ const getContent = (xmlFile) => {
   return fxp.parse(xmlContent.toString(), fxpOptions);
 };
 
-const getProjectPaths = productPath => {
-
+const getProjectPaths = (productPath) => {
   const cases = {
     recentProjectDirectories: () => {
       // //component[@name='RecentDirectoryProjectsManager']/option[@name='recentPaths']/list/option/@value
-      const content = getContent(`${productPath}/options/recentProjectDirectories.xml`);
+      const content = getContent(
+        `${productPath}/options/recentProjectDirectories.xml`
+      );
       if (content) {
         const application = content.application;
-        const options = application.component && application.component['@_name'] === 'RecentDirectoryProjectsManager' ? application.component.option : [];
-        const recentPathOptions = options.find(option => {
-          return option['@_name'] === 'recentPaths';
+        const options =
+          application.component &&
+          application.component["@_name"] === "RecentDirectoryProjectsManager"
+            ? application.component.option
+            : [];
+        const recentPathOptions = options.find((option) => {
+          return option["@_name"] === "recentPaths";
         });
 
-        return (recentPathOptions ? recentPathOptions.list.option : []);
+        return recentPathOptions ? recentPathOptions.list.option : [];
       }
       return false;
     },
@@ -36,12 +41,30 @@ const getProjectPaths = productPath => {
       const content = getContent(`${productPath}/options/recentProjects.xml`);
       if (content) {
         const application = content.application;
-        const options = application.component && application.component['@_name'] === 'RecentProjectsManager' ? application.component.option : [];
-        const recentPathOptions = options.find(option => {
-          return option['@_name'] === 'recentPaths';
+        let options =
+          application.component &&
+          application.component["@_name"] === "RecentProjectsManager"
+            ? application.component.option
+            : [];
+        // 2020.3+
+        if (typeof options === "object") {
+          // there is only one entry in 2020.3+, so we change it into array
+          options = [options];
+        }
+        const recentPathOptions = options.find((option) => {
+          return option["@_name"] === "recentPaths";
         });
-
-        return (recentPathOptions ? recentPathOptions.list.option : []);
+        if (recentPathOptions) {
+          return recentPathOptions.list.option;
+        }
+        // 2020.3+
+        const entries = options.find((option) => {
+          return option["@_name"] === "additionalInfo";
+        });
+        if (entries) {
+          return entries.map.entry;
+        }
+        return [];
       }
       return false;
     },
@@ -50,12 +73,16 @@ const getProjectPaths = productPath => {
       const content = getContent(`${productPath}/options/recentSolutions.xml`);
       if (content) {
         const application = content.application;
-        const options = application.component && application.component['@_name'] === 'RecentDirectoryProjectsManager' ? application.component.option : [];
-        const recentPathOptions = options.find(option => {
-          return option['@_name'] === 'recentPaths';
+        const options =
+          application.component &&
+          application.component["@_name"] === "RecentDirectoryProjectsManager"
+            ? application.component.option
+            : [];
+        const recentPathOptions = options.find((option) => {
+          return option["@_name"] === "recentPaths";
         });
 
-        return (recentPathOptions ? recentPathOptions.list.option : []);
+        return recentPathOptions ? recentPathOptions.list.option : [];
       }
       return false;
     },
@@ -67,12 +94,24 @@ const getProjectPaths = productPath => {
       if (rawProjectPaths) {
         const projectPaths = [];
 
-        rawProjectPaths = Object.prototype.toString.call(rawProjectPaths).indexOf('Object') !== -1
-          ? [rawProjectPaths] // one project case
-          : rawProjectPaths;
+        rawProjectPaths =
+          Object.prototype.toString.call(rawProjectPaths).indexOf("Object") !==
+          -1
+            ? [rawProjectPaths] // one project case
+            : rawProjectPaths;
 
-        rawProjectPaths.forEach(project => {
-          projectPaths.push(project['@_value'].replace('$USER_HOME$', process.env.HOME));
+        rawProjectPaths.forEach((project) => {
+          if (project["@_value"]) {
+            projectPaths.push(
+              project["@_value"].replace("$USER_HOME$", process.env.HOME)
+            );
+          }
+          // 2020.3+
+          if (project["@_key"]) {
+            projectPaths.push(
+              project["@_key"].replace("$USER_HOME$", process.env.HOME)
+            );
+          }
         });
 
         return projectPaths;
